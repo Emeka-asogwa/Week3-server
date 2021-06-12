111de50fff16f57e501a35b331b85bb80b75e6e4
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-12 23:02:15 +0000

    ✍🏽 Depth First Traversal

--- vampr-js/vampire.js
+++ vampr-js/vampire.js
@@ -40,6 +40,82 @@ class Vampire {
   closestCommonAncestor(vampire) {
 
   }
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+    else {
+      return null;
+    }
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let numberOfVampire = 0;
+
+    numberOfVampire += this.offspring.length;
+
+    for (const descendent of this.offspring) {
+      let lengthOfDescendent = descendent.totalDescendents;
+      numberOfVampire += lengthOfDescendent;
+    }
+    return numberOfVampire;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millenialVampire = [];
+    if (this.yearConverted >= 2000) {
+      millenialVampire.push(this);
+    }
+
+    for (const descendent of this.offspring) {
+      let millenialDescendent = descendent.allMillennialVampires;
+      millenialVampire.push(millenialDescendent);
+    }
+    return millenialVampire;
+  }
+
+  /** Tree traversal methods **/
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+    else {
+      return null;
+    }
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let numberOfVampire = 0;
+    numberOfVampire += this.offspring.length;
+
+    for (const descendent of this.offspring) {
+      let numberOfDescendent = descendent.totalDescendents;
+      numberOfVampire += numberOfDescendent;
+    }
+    return numberOfVampire;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millenialVampire = [];
+    if (this.yearConverted >= 2000) {
+      millenialVampire.push(this);
+    }
+
+    for (const descendent of this.offspring) {
+      let millenialDescendent = descendent.allMillennialVampires;
+      millenialVampire.push(millenialDescendent)
+    }
+    return millenialVampire;
+  }
+
+
 }
 
 module.exports = Vampire;
